5f058d30f37ab961221de0a10992c1b36a5d1140
cornelk <github@codejuggle.dj>
2022-10-18 22:36:57 -0600

    database: add connection pool support

--- database/database.go
+++ database/database.go
@@ -10,13 +10,22 @@ import (
 	"github.com/georgysavva/scany/v2/pgxscan"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
 	_ "github.com/jackc/pgx/v5/stdlib" // register as sql driver
 	"github.com/jackc/pgx/v5/tracelog"
 )
 
 // Connection defines a database connection. It exposes the exported functions of both embedded types.
+// The connection is not concurrency-safe.
 type Connection struct {
 	*pgx.Conn
+	*pgxscan.API
+}
+
+// Pool defines a connection pool. It exposes the exported functions of both embedded types.
+// The pool is concurrency-safe.
+type Pool struct {
+	*pgxpool.Pool
 	*pgxscan.API
 }
 
@@ -94,4 +103,53 @@ func NewStdlib(ctx context.Context, cfg Config) (*sql.DB, error) {
 	}
 
 	return db, nil
+}
+
+// NewPool establishes a connection pool with a PostgreSQL database.
+func NewPool(ctx context.Context, cfg Config) (*Pool, error) {
+	if err := cfg.Validate(); err != nil {
+		return nil, fmt.Errorf("validating config: %w", err)
+	}
+
+	hostPort := net.JoinHostPort(cfg.Host, cfg.Port)
+	url := fmt.Sprintf("postgres://%s:%s@%s/%s", cfg.User, cfg.Password, hostPort, cfg.Database)
+	connConfig, err := pgxpool.ParseConfig(url) // nolint: contextcheck
+	if err != nil {
+		return nil, fmt.Errorf("parsing config: %w", err)
+	}
+	if cfg.Logger != nil {
+		connConfig.ConnConfig.Tracer = &tracelog.TraceLog{
+			Logger:   cfg.Logger,
+			LogLevel: cfg.Logger.Level(),
+		}
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, connConfig)
+	if err != nil {
+		return nil, fmt.Errorf("connecting to database: %w", err)
+	}
+
+	scan, err := pgxscan.NewDBScanAPI()
+	if err != nil {
+		return nil, fmt.Errorf("creating db scan api: %w", err)
+	}
+	scanAPI, err := pgxscan.NewAPI(scan)
+	if err != nil {
+		return nil, fmt.Errorf("creating scan api: %w", err)
+	}
+
+	conn := &Pool{
+		Pool: pool,
+		API:  scanAPI,
+	}
+	return conn, nil
+}
+
+// Close closes all connections in the pool and rejects future Acquire calls.
+// Blocks until all connections are returned to pool and closed.
+// Implement the same Close function signature to allow Pool and Connection to have the
+// same function signatures.
+func (p *Pool) Close(_ context.Context) error {
+	p.Pool.Close()
+	return nil
 }
