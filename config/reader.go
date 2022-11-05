d5bf8ec08ea44141001828be9b7a5c26ce1ead18
cornelk <github@codejuggle.dj>
2022-11-05 10:57:19 -0600

    config: allow passing of multiple prefixes to the reader

--- config/reader.go
+++ config/reader.go
@@ -10,20 +10,22 @@ import (
 
 // Read reads the environment variables for the given prefix and unmarshals it into the config object.
 // To support both prefixed and non prefixed envs at the same time it is recommended to call the function
-// first without prefix and a second time with the prefix set. Only environment variables that exist will
+// with an empty first prefix and a second set prefix. Only environment variables that exist will
 // set a field in the config. This way, an environment variable set without a prefix can be overwritten
 // by an environment variable with a prefix.
-func Read(prefix string, config any) error {
-	if prefix != "" {
-		if !strings.HasSuffix(prefix, "_") {
-			prefix += "_"
+func Read(config any, prefixes ...string) error {
+	for _, prefix := range prefixes {
+		if prefix != "" {
+			if !strings.HasSuffix(prefix, "_") {
+				prefix += "_"
+			}
+			prefix = strings.ToUpper(prefix)
 		}
-		prefix = strings.ToUpper(prefix)
-	}
 
-	opts := env.Options{Prefix: prefix}
-	if err := env.Parse(config, opts); err != nil {
-		return fmt.Errorf("reading config from env: %w", err)
+		opts := env.Options{Prefix: prefix}
+		if err := env.Parse(config, opts); err != nil {
+			return fmt.Errorf("reading config from env: %w", err)
+		}
 	}
 	return nil
 }
--- config/reader_test.go
+++ config/reader_test.go
@@ -16,11 +16,15 @@ func TestReader(t *testing.T) {
 		Database Database `envPrefix:"DATABASE_"`
 	}
 
+	t.Setenv("DATABASE_HOST", "defaulthost")
 	t.Setenv("TESTAPP_DATABASE_HOST", "localhost")
 
 	var cfg Config
-	require.NoError(t, Read("testapp", &cfg))
+	require.NoError(t, Read(&cfg, "", "testapp"))
 	assert.Equal(t, "localhost", cfg.Database.Host)
 
-	require.Error(t, Read("", cfg)) // not passing a pointer fails
+	require.NoError(t, Read(&cfg, "testapp", ""))
+	assert.Equal(t, "defaulthost", cfg.Database.Host)
+
+	require.Error(t, Read(cfg, "")) // not passing a pointer fails
 }
