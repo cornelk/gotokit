cb4193a4e02b8d6a24f9f691070e62ab21787f70
cornelk <github@codejuggle.dj>
2024-06-25 17:52:56 -0600

    log: add CloserCtx function

--- log/closer.go
+++ log/closer.go
@@ -1,19 +1,35 @@
 package log
 
 import (
+	"context"
 	"errors"
 	"io"
 	"os"
 )
 
-// CloserLogOnError calls the closer function and if an error gets returned it logs an error.
+// Closer calls the closer function and if an error gets returned it logs an error.
 // This function is useful when using patterns like defer resp.Body.Close() which now become:
-// defer logger.CloserLogOnError(resp.Body, "closing body").
-func (l *Logger) CloserLogOnError(closer io.Closer, msg string) {
+// defer logger.Closer(resp.Body, "closing body").
+func (l *Logger) Closer(closer io.Closer, msg string) {
 	err := closer.Close()
 	if err == nil || errors.Is(err, os.ErrClosed) {
 		return
 	}
 
 	l.Error(msg, Err(err))
+}
+
+// closerCtx is the interface that wraps the extended Close method.
+type closerCtx interface {
+	Close(ctx context.Context) error
+}
+
+// CloserCtx calls the closer function and if an error gets returned it logs an error.
+func (l *Logger) CloserCtx(ctx context.Context, closer closerCtx, msg string) {
+	err := closer.Close(ctx)
+	if err == nil || errors.Is(err, os.ErrClosed) {
+		return
+	}
+
+	l.ErrorContext(ctx, msg, Err(err))
 }
--- log/closer_test.go
+++ log/closer_test.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"bytes"
+	"context"
 	"errors"
 	"strings"
 	"testing"
@@ -19,7 +20,7 @@ func (t testCloser) Close() error {
 	return t.err
 }
 
-func TestLoggerCloserLogOnError(t *testing.T) {
+func TestLoggerCloser(t *testing.T) {
 	cfg, err := ConfigForEnv(env.Development)
 	require.NoError(t, err)
 	var buf bytes.Buffer
@@ -32,14 +33,50 @@ func TestLoggerCloserLogOnError(t *testing.T) {
 	closer := testCloser{}
 	msg := "closing failed"
 
-	logger.CloserLogOnError(closer, msg)
+	logger.Closer(closer, msg)
 	output := buf.String()
 	assert.False(t, strings.Contains(output, "ERROR"))
 	assert.False(t, strings.Contains(output, msg))
 
 	errMsg := "failure"
 	closer.err = errors.New(errMsg)
-	logger.CloserLogOnError(closer, msg)
+	logger.Closer(closer, msg)
+	output = buf.String()
+	assert.True(t, strings.Contains(output, "ERROR"))
+	assert.True(t, strings.Contains(output, msg))
+	assert.True(t, strings.Contains(output, errMsg))
+}
+
+type testCloserCtx struct {
+	err error
+}
+
+func (t testCloserCtx) Close(context.Context) error {
+	return t.err
+}
+
+func TestLoggerCloserCtx(t *testing.T) {
+	cfg, err := ConfigForEnv(env.Development)
+	require.NoError(t, err)
+	var buf bytes.Buffer
+	cfg.Output = &buf
+	cfg.TimeFormat = "-"
+
+	logger, err := NewWithConfig(cfg)
+	require.NoError(t, err)
+
+	ctx := context.Background()
+	closer := testCloserCtx{}
+	msg := "closing failed"
+
+	logger.CloserCtx(ctx, closer, msg)
+	output := buf.String()
+	assert.False(t, strings.Contains(output, "ERROR"))
+	assert.False(t, strings.Contains(output, msg))
+
+	errMsg := "failure"
+	closer.err = errors.New(errMsg)
+	logger.CloserCtx(ctx, closer, msg)
 	output = buf.String()
 	assert.True(t, strings.Contains(output, "ERROR"))
 	assert.True(t, strings.Contains(output, msg))
